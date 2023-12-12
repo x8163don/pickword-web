8815faec2c10e924966af8ca22acf68dc13d7407
Bevis <x8163don@gmail.com>
2023-12-12 17:18:35 +0800

    feat: add account page

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,12 +1,17 @@
 import store from "../../store";
 import {fetchAccountDetail} from "../../store/account/action";
 import {useSelector} from "react-redux";
+import {Typography, Avatar} from "@material-tailwind/react";
 
 export default function Account() {
 
     const account = useSelector(state => state.account.account)
 
-    return <div>Account</div>
+    return <div>
+        <Avatar src={account.avatar} alt="avatar" size="xl"/>
+        <Typography>{account.name}</Typography>
+        <Typography>{account.email}</Typography>
+    </div>
 }
 
 export const accountLoader = () => {
