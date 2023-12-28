7d595ddc705a7742858043ce815158f712a54451
Bevis <x8163don@gmail.com>
2023-12-28 10:46:05 +0800

    feat: add active style to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,6 @@
 import {
     Card,
     Typography,
-    List,
-    ListItem,
-    ListItemPrefix,
 } from "@material-tailwind/react";
 import {
     HomeIcon,
@@ -12,45 +9,45 @@ import {
     UserCircleIcon,
     VideoCameraIcon
 } from "@heroicons/react/24/solid";
-import {useNavigate} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import Logo from "../assets/logo/logo.png";
 
 export default function Sidebar() {
 
-    const navigate = useNavigate();
-
     const items = [
-        {text: "主頁", icon: <HomeIcon className="h-5 w-5"/>, page: "/dashboard"},
-        {text: "影片", icon: <VideoCameraIcon className="h-5 w-5"/>, page: "/content"},
-        {text: "字卡", icon: <Square2StackIcon className="h-5 w-5"/>, page: "/word"},
-        {text: "複習", icon: <RocketLaunchIcon className="h-5 w-5"/>, page: "/review"},
-        {text: "帳戶", icon: <UserCircleIcon className="h-5 w-5"/>, page: "/account"},
+        {text: "主頁", icon: <HomeIcon className="h-6 w-6"/>, page: "/dashboard"},
+        {text: "影片", icon: <VideoCameraIcon className="h-6 w-6"/>, page: "/content"},
+        {text: "字卡", icon: <Square2StackIcon className="h-6 w-6"/>, page: "/word"},
+        {text: "複習", icon: <RocketLaunchIcon className="h-6 w-6"/>, page: "/review"},
+        {text: "帳戶", icon: <UserCircleIcon className="h-6 w-6"/>, page: "/account"},
     ]
 
-    return <Card className="h-[calc(100vh)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 bg-gray-100 rounded-none">
+    return <Card
+        className="h-[calc(100vh)] w-full max-w-[16rem] p-4 shadow-xl shadow-blue-gray-900/5 bg-gray-100 rounded-none">
         <div className="mb-2 flex items-center gap-4 p-4">
             <img src={Logo} alt="brand" className="h-8 w-8"/>
             <Typography variant="h5" color="blue-gray">
                 PickWord
             </Typography>
         </div>
-        <List>
+
+        <div className="mt-4 flex flex-col gap-6">
             {
                 items.map((item, index) => (
-                    <ListItem
+                    <NavLink
+                        exact
                         key={item.text}
-                        onClick={() => {
-                            navigate(item.page)
-                        }}
+                        to={item.page}
+                        className={({isActive}) => (isActive ? "bg-gray-400 rounded-md text-gray-50" : "") +" py-2 px-4"}
                     >
-                        <ListItemPrefix>
+                        <div className="flex items-center gap-4">
                             {item.icon}
-                        </ListItemPrefix>
-                        {item.text}
-                    </ListItem>
+                            <Typography variant="h6">{item.text}</Typography>
+                        </div>
+                    </NavLink>
                 ))
             }
-        </List>
+        </div>
     </Card>
 
 }
