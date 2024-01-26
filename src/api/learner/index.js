4e7cbe561befa9eb4ebe120e33dccdbe5c083e70
Bevis <x8163don@gmail.com>
2024-01-26 18:45:14 +0800

    feat: add mastered search feature

--- src/api/learner/index.js
+++ src/api/learner/index.js
@@ -1,6 +1,6 @@
 import {getAuthToken} from "../../utils/auth";
 
-export const searchFollowedWords = async ({page, limit = 12, text, signal}) => {
+export const searchFollowedWords = async ({page, limit = 12, text, is_mastered, signal}) => {
     const token = getAuthToken();
     const url = new URL(`${process.env.REACT_APP_BASE_URL}followedWord:search`)
     if (text) {
@@ -8,6 +8,9 @@ export const searchFollowedWords = async ({page, limit = 12, text, signal}) => {
     }
     url.searchParams.set("page", page)
     url.searchParams.set("limit", limit)
+    if (is_mastered !== undefined) {
+        url.searchParams.set("is_mastered", is_mastered)
+    }
 
     const response = await fetch(url, {
         method: 'GET',
@@ -22,6 +25,17 @@ export const getLearningProgress = async ({signal}) => {
     const response = await fetch(`${process.env.REACT_APP_BASE_URL}learner:progress`, {
         method: 'GET',
         headers: {Authorization: token},
+        signal
+    })
+    return response.json()
+}
+
+export const changeMasteredWord = async ({wordID, isMastered, signal}) => {
+    const token = getAuthToken();
+    const response = await fetch(`${process.env.REACT_APP_BASE_URL}learner:master_word`, {
+        method: 'POST',
+        headers: {Authorization: token, 'Content-Type': 'application/json'},
+        body: JSON.stringify({word_id: wordID, is_mastered: isMastered}),
         signal
     })
     return response.json()
